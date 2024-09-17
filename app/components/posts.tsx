ec76e5b68f88902997be31bac5a816e60599ffea
aealni <102875374+aealni@users.noreply.github.com>
2024-09-16 22:08:56 -0500

    aa

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -25,7 +25,8 @@ export function ProjectsPosts() {
               <p className="text-neutral-600 dark:text-neutral-400 w-[150px] tabular-nums">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">{post.metadata.title}
+              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+                {post.metadata.title}
               </p>
             </div>
           </Link>
