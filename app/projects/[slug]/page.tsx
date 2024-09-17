5b60cbaf15c2f85fa6a2c673b14f00e7539f782a
aealni <102875374+aealni@users.noreply.github.com>
2024-09-16 22:05:20 -0500

    sa

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -82,8 +82,7 @@ export default function Projects({ params }) {
           }),
         }}
       />
-      <h1 className="title font-semibold text-2xl tracking-tighter">
-        {post.metadata.title}
+      <h1 className="title font-semibold text-2xl tracking-tighter">{post.metadata.title}
       </h1>
       <div className="flex justify-between items-center mt-2 mb-8 text-sm">
         <p className="text-sm text-neutral-600 dark:text-neutral-400">
