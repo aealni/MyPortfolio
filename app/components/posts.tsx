bfcb0a899283fbffdb7cf9b083481b49ed37720a
aealni <102875374+aealni@users.noreply.github.com>
2024-09-16 21:37:10 -0500

    idk helping a lil ?

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -21,7 +21,7 @@ export function ProjectsPosts() {
             className="flex flex-col space-y-1 mb-4"
             href={`/projects/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+            <div className="w-full flex flex-col md:flex-row items-start md:items-center space-x-0 md:space-x-2">
               <p className="text-neutral-600 dark:text-neutral-400 w-[150px] tabular-nums">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
