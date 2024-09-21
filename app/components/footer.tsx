2e64d0e743f277a6c99813883fa83e7654380646
aealni <102875374+aealni@users.noreply.github.com>
2024-09-20 23:16:07 -0500

    cleanup

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://drive.google.com/file/d/1r4Dz5ePkwHXmOAxqtvBQ-GeH2lUSQzuV/preview"
+            href="https://aealni.vercel.app/resume_JL.pdf"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">resume</p>
